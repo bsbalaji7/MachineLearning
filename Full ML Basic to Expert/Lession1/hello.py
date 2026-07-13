db6bccb9bbbf40e4cccccdd3186ab9a486517bed
BS <bsbalaji101005@gmail.com>
2026-07-13 19:39:27 +0530

    Installing the Matplotlib, pandas and etc

---
+++ Full ML Basic to Expert/Lession1/hello.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+
+print("Welcome BS!")
+print("Machine Learning Journey Starts Today")
+
+print("\nInstalled Versions")
+print("------------------")
+print("NumPy:", np.__version__)
+print("Pandas:", pd.__version__)
+print("Scikit-Learn:", sklearn.__version__)
