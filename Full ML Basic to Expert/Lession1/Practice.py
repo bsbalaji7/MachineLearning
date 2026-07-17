30d0823d384b2fed46cd7b952d5a5c4c0b73ffbb
BS <bsbalaji101005@gmail.com>
2026-07-17 09:19:23 +0530

    Create Practice.py

---
+++ Full ML Basic to Expert/Lession1/Practice.py
@@ -0,0 +1,19 @@
+#creating array
+import numpy as np
+arr1 = np.array([1,2,4,5,6]);
+print(arr1);
+
+#creating marksheet in array and size, shape, Data type
+mrks = np.array([90,89,79,85,100,100])
+
+print(mrks.shape)
+print(mrks.size)
+print(mrks.dtype)
+
+#Create a 2×3 array
+dim = np.array([
+    [20,24,23],
+    [100,200,300]
+    ])
+print(dim.shape)
+print(dim.ndim)
